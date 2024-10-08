d03b1cfe5ee54389c7be9d7f967a67a2007a36fc
Hypatia Garcia <Beatrizmaciel28005@gmail.com>
2024-10-08 05:32:29 +0200

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -6,6 +6,6 @@ def cargar_datos(ruta_archivo):
     return datos
 
 if __name__ == "__main__":
-    ruta_archivo = '../data/retail_sales.csv'
+    ruta_archivo = 'https://github.com/hypatiagarcia/retail-sales-analysis/blob/main/data/retail_sales_dataset.csv'
     datos = cargar_datos(ruta_archivo)
     print(datos)
